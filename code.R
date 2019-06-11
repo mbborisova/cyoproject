766886096536efaecdfa88761dfa4fd128b244c4
Maggie Borisova <maggie@phalangy.birgenshire.net>
2019-06-11 02:13:09 -0700

    adding code.R

---
+++ code.R
@@ -0,0 +1,3 @@
+
+cacao <- read.csv("/Users/maggie/Documents/code/cyoproject/chocolate/flavors_of_cacao.csv", 
+         header = TRUE, sep = ",", quote = "\"", dec = ".", fill = TRUE, comment.char = "")
